71040a322431a4b25dab0a26b616e99594ce7cbe
migmadee <migmademasherpa079@gmail.com>
2025-05-20 03:38:32 -0700

    first commit

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,13 @@
+// routes/authRoutes.ts
+import express from 'express'
+import { loginUser, registerUser, getProfile } from '../controller/authcontroller'
+import { authenticate } from '../middleware/authMiddleware'
+
+const router = express.Router()
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/me', authenticate,getProfile )
+
+
+export default router
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from './app'
+import connectDB from './config/db'
+
+connectDB()
+const port = process.env.PORT || 3001
+
+//  Start Server
+app.listen(port, () => {
+    console.log(`Server running on port ${port} `)
+})
